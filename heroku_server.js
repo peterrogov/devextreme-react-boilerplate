d0c80f66350b10336ac1000a9c79a7be52f45e5d
Peter Rogov <peterrogov@gmail.com>
2022-01-07 16:07:47 +0300

    heroku server

---
+++ heroku_server.js
@@ -0,0 +1,29 @@
+/**
+ * https://gist.github.com/ryanoglesby08/1e1f49d87ae8ab2cabf45623fc36a7fe
+ */
+
+/*
+  Incredibly simple Node.js and Express application server for serving static assets.
+  
+  DON'T USE THIS IN PRODUCTION!
+  
+    It is meant for learning purposes only. This server is not optimized for performance, 
+    and is missing key features such as error pages, compression, and caching.
+
+*/
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+// serve static assets normally
+app.use(express.static(__dirname + '/build'));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, '0.0.0.0');
