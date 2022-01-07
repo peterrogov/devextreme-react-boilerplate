f7c55c49c514b5974d3e4198de2d9367b90eae56
Peter Rogov <peterrogov@gmail.com>
2022-01-07 16:25:15 +0300

    logging

--- heroku_server.js
+++ heroku_server.js
@@ -6,7 +6,7 @@
   Incredibly simple Node.js and Express application server for serving static assets.
   
   DON'T USE THIS IN PRODUCTION!
-  
+
     It is meant for learning purposes only. This server is not optimized for performance, 
     and is missing key features such as error pages, compression, and caching.
 
@@ -17,6 +17,8 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
+console.log("the app is starting on port", port);
+
 // serve static assets normally
 app.use(express.static(__dirname + '/build'));
 
@@ -26,4 +28,6 @@ app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0');
+app.listen(port, () => {
+    console.log("the app is listening on port", port);
+});
