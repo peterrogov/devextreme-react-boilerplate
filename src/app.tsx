2de752d4dbf501212fb6c8a2fc6938afe15e1dd3
Peter Rogov <peterrogov@gmail.com>
2022-01-07 21:28:54 +0300

    import fix

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './themes/light/theme.scss';
 import AppRoot from './layout/main';
-import { setupFontAwesome } from './fontAwesome';
+import setupFontAwesome from './fontAwesome';
 
 setupFontAwesome();
 
